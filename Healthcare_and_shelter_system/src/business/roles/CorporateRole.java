b6b9771cf7f64aa0121cc0591ebbb85f1849444c
survea <surve.a@northeastern.edu>
2021-08-04 19:31:46 +0530

    Updated UI and model of Corporate Role

--- Healthcare_and_shelter_system/src/business/roles/CorporateRole.java
+++ Healthcare_and_shelter_system/src/business/roles/CorporateRole.java
@@ -7,10 +7,22 @@
  */
 package business.roles;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.network.Network;
+import business.organization.Organization;
+import business.userAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author dhairyasheel
  */
-public class CorporateRole {
+public class CorporateRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Network network, EcoSystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
